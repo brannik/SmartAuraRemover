a8ff0c8ebb0adf2d47180a7a5ce61a794d9d5bdb
Georgi Golemshinski <georgi.vtu.mi@gmail.com>
2025-01-02 19:59:52 +0200

    auras fix
    auras update

--- AuraList.lua
+++ AuraList.lua
@@ -3,4 +3,7 @@ BDW_TRINKET_AURAS = {
     { id = 71560, name = "Speed of the Vrykul" },
     { id = 71556, name = "Agility of the Vrykul" },
     { id = 71558, name = "Power of the Taunka" },
+    { id = 71599, name = "Aim if the Iron Dwarves" },
+    { id = 71561, name = "Strength of the Taunka" },
 }
+ 
